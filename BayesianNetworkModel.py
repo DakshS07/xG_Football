c477b072c8d308d01df52926a8f8bdbda7b2481a
DakshS07 <86058519+DakshS07@users.noreply.github.com>
2021-06-27 11:25:50 +0530

    Update BayesianNetworkModel.py

--- BayesianNetworkModel.py
+++ BayesianNetworkModel.py
@@ -123,3 +123,6 @@ q15 = Xg_inference.query(variables=['is_goal'], evidence={'location': 9, 'body_p
 
 print(q1+q2+q3+q4+q5+q6+q7+q8+q9+q10+q11+q12+q13+q14+q15)
 
+# marginal inference - P(body part | assist).
+
+print(Xg_inference.query(variables=['body_part'], evidence={'assist_type': 2}))
